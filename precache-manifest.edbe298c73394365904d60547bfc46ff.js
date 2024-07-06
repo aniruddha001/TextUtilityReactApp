f8dae309d404afeb413eb44e568b9e301cee2d19
aniruddha001 <aniruddhamahamulkar@gmail.com>
2024-07-06 17:30:55 +0530

    Updates

---
+++ precache-manifest.edbe298c73394365904d60547bfc46ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d302e893f527ba0018125d79e79b4e14",
+    "url": "/aniruddha001/TextUtilityReactApp.git/index.html"
+  },
+  {
+    "revision": "c9e23f35d1032a4ed725",
+    "url": "/aniruddha001/TextUtilityReactApp.git/static/css/main.ea5ca415.chunk.css"
+  },
+  {
+    "revision": "c72feb6dc286339a449b",
+    "url": "/aniruddha001/TextUtilityReactApp.git/static/js/2.38fec76d.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/aniruddha001/TextUtilityReactApp.git/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "c9e23f35d1032a4ed725",
+    "url": "/aniruddha001/TextUtilityReactApp.git/static/js/main.170c3241.chunk.js"
+  },
+  {
+    "revision": "4229a688ffbe101f66b9",
+    "url": "/aniruddha001/TextUtilityReactApp.git/static/js/runtime~main.0f58a730.js"
+  }
+]);
